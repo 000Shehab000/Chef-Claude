c7900dfdd74e8bc2b53ed483ef86486b157ef2e3
000Shehab000 <shhab200025@gmail.com>
2025-07-04 19:06:00 +0300

    refactor adding ingredients

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -3,21 +3,18 @@ import React from 'react'
 export default function Main() {
   const [ingredients, setIngredinets] = React.useState(['tomato'])
 
-  // const ingredients = ['tomato', 'onion', 'apple']
   const ingredientItems = ingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>
   })
 
-  function handleSubmit(event) {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
+  function addIngredient(formData) {
     const newIngredient = formData.get('ingredient')
     setIngredinets((prevIngredinets) => [...prevIngredinets, newIngredient])
   }
 
   return (
     <main>
-      <form onSubmit={handleSubmit}>
+      <form action={addIngredient}>
         <input
           type="text"
           placeholder="e.g. oregano"
