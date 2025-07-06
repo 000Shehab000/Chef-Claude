6084873ee87ea6c657e682a000e5a778adc13451
000Shehab000 <shhab200025@gmail.com>
2025-07-06 21:04:14 +0300

    adding getting recipe section

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Main() {
-  const [ingredients, setIngredinets] = React.useState(['tomato'])
+  const [ingredients, setIngredinets] = React.useState([])
 
   const ingredientItems = ingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>
@@ -23,7 +23,23 @@ export default function Main() {
         />
         <button>+ Add ingredient</button>
       </form>
-      <ul>{ingredientItems}</ul>
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientItems}
+          </ul>
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
     </main>
   )
 }
